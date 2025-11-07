bd3ebb309f69e1c2265b80f16f86ef65f6b35041
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-11-07 08:57:40 +0800

    final

---
+++ binaryCalculatorAndConversion.js
@@ -0,0 +1,42 @@
+alert("Welcome to Binary Calculator & Converter!");
+let firstValueSum = prompt("Enter the first binary value to add:");
+let secondValueSum = prompt("Enter the second binary value to add:");
+let sum = addBinaryFunction(firstValueSum, secondValueSum);
+function addBinaryFunction(a, b) {
+    firstAddend = parseInt(a, 2)
+    secondAddend = parseInt(b, 2)
+    return firstAddend + secondAddend;
+}
+const binarySum = sum.toString(2);
+console.log(firstValueSum + " & " + secondValueSum + "'s Binary Sum: " + binarySum);
+
+let firstValueDifference = prompt("Enter the first binary value to subtract:");
+let secondValueDifference = prompt("Enter the second binary value to subtract:");
+let difference = subtractBinaryFunction(firstValueDifference, secondValueDifference);
+function subtractBinaryFunction(a, b) {
+    minuend = parseInt(a, 2)
+    subtrahend = parseInt(b, 2)
+    return minuend - subtrahend;
+}
+
+if (difference >= 0) {
+    console.log(firstValueDifference + " & " + secondValueDifference + "'s Binary Difference: " + difference.toString(2));  
+} else {
+    console.log(firstValueDifference + " & " + secondValueDifference +  "'s Binary Difference: -" + (-difference).toString(2));
+    // to handle and avoid negative results appearing as full 32-bit two’s complement especially when they're large values
+}
+
+let binaryNumber = prompt("Enter a binary value you'd want to convert in Octal, Decimal, and Hexadecimal values:")
+let convertedNumber = convertBinaryFunction(binaryNumber);
+function convertBinaryFunction(a) {
+    const decimalValue = parseInt(a, 2);
+    console.log(a + " in Decimal: " + decimalValue);
+
+    const octalValue = decimalValue.toString(8);
+    console.log(a + " in Octal: " + octalValue);
+
+    const hexadecimalValue = decimalValue.toString(16);
+    console.log(a + " in Hexadecimal: " + hexadecimalValue.toUpperCase());
+}
+
+alert("Inspect and check the console to see the results!");
