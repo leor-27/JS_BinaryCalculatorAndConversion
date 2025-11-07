5c08abbde0d1fd7914efed4fde51368f66c10b0b
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-11-07 12:11:56 +0800

    final fr fr

--- binaryCalculatorAndConversion.js
+++ binaryCalculatorAndConversion.js
@@ -3,20 +3,20 @@ let firstValueSum = prompt("Enter the first binary value to add:");
 let secondValueSum = prompt("Enter the second binary value to add:");
 let sum = addBinaryFunction(firstValueSum, secondValueSum);
 function addBinaryFunction(a, b) {
-    const firstAddend = parseInt(a, 2)
-    const secondAddend = parseInt(b, 2)
-    return firstAddend + secondAddend;
+    const FIRST_ADDEND = parseInt(a, 2)
+    const SECOND_ADDEND = parseInt(b, 2)
+    return FIRST_ADDEND + SECOND_ADDEND;
 }
-const binarySum = sum.toString(2);
-console.log(firstValueSum + " & " + secondValueSum + "'s Binary Sum: " + binarySum);
+const BINARY_SUM = sum.toString(2);
+console.log(firstValueSum + " & " + secondValueSum + "'s Binary Sum: " + BINARY_SUM);
 
 let firstValueDifference = prompt("Enter the first binary value to subtract:");
 let secondValueDifference = prompt("Enter the second binary value to subtract:");
 let difference = subtractBinaryFunction(firstValueDifference, secondValueDifference);
 function subtractBinaryFunction(a, b) {
-    const minuend = parseInt(a, 2)
-    const subtrahend = parseInt(b, 2)
-    return minuend - subtrahend;
+    const MINUEND = parseInt(a, 2)
+    const SUBTRAHEND = parseInt(b, 2)
+    return MINUEND - SUBTRAHEND;
 }
 
 if (difference >= 0) {
@@ -29,14 +29,14 @@ if (difference >= 0) {
 let binaryNumber = prompt("Enter a binary value you'd want to convert in Octal, Decimal, and Hexadecimal values:")
 convertBinaryFunction(binaryNumber);
 function convertBinaryFunction(a) {
-    const decimalValue = parseInt(a, 2);
-    console.log(a + " in Decimal: " + decimalValue);
+    const DECIMAL_VALUE = parseInt(a, 2);
+    console.log(a + " in Decimal: " + DECIMAL_VALUE);
 
-    const octalValue = decimalValue.toString(8);
-    console.log(a + " in Octal: " + octalValue);
+    const OCTAL_VALUE = decimalValue.toString(8);
+    console.log(a + " in Octal: " + OCTAL_VALUE);
 
-    const hexadecimalValue = decimalValue.toString(16);
-    console.log(a + " in Hexadecimal: " + hexadecimalValue.toUpperCase());
+    const HEXADECIMAL_VALUE = decimalValue.toString(16);
+    console.log(a + " in Hexadecimal: " + HEXADECIMAL_VALUE.toUpperCase());
 }
 
 alert("Inspect and check the console to see the results!");
